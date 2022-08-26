c8a099a2e6727770458b75b80506f0f781e7f7ec
Tiberius Lockett <tlockett680@gmail.com>
2022-08-26 13:13:17 -0700

    Make sizePrice pass test 1

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,17 @@ this.size = size
 this.sauce = sauce
 this.dipping = dip
 this.toppings = [];
+this.price = 0;
+}
+
+Pizza.prototype.sizePrice = function() {
+  this.size = this.size;
+  if(this.size === "small") {
+    this.price = 8;
+  } else if (this.size === 'medium') {
+    this.price = 10;
+  } else {
+    this.price = 12
+  }
+  return this.price
 }
