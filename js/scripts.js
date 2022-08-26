f8c4c17f6975c70012b7aec4f36ce9562e5730a6
Tiberius Lockett <tlockett680@gmail.com>
2022-08-26 13:36:19 -0700

    Add tests for saucePrice and dipping price

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,37 @@ this.price = 0;
 
 Pizza.prototype.sizePrice = function() {
   this.size = this.size;
-  if(this.size === "small") {
+  if(this.size === 'Small') {
     this.price = 8;
-  } else if (this.size === 'medium') {
+  } else if (this.size === 'Medium') {
     this.price = 10;
   } else {
     this.price = 12
   }
   return this.price
 }
+
+Pizza.prototype.saucePrice = function() {
+  this.sauce = this.sauce;
+  if(this.sauce === 'Red Sauce') {
+    this.price += 2;
+  } else if (this.size === 'White Sauce') {
+    this.price += 3;
+  } else {
+    this.price += 0
+  }
+  return this.price
+}
+
+Pizza.prototype.dippingPrice = function() {
+  this.dipping = this.dipping;
+  if(this.dipping === 'Ranch') {
+    this.price += 2;
+  } else if (this.dipping === 'Marinara') {
+    this.price += 1;
+  } else {
+    this.price += 0
+  }
+  return this.price
+}
+
--- readme.md
+++ readme.md
@@ -14,5 +14,23 @@ Describe sizePrice()
 
 Test: "It should return price of pizza depending on size that was selected"
 Code:
-  let pizza = new Pizza("Large")
+  let pizza = new Pizza("Small", "Red Sauce", "Ranch")
   pizza.sizePrice
+Expected Return: 8
+
+Describe saucePrice()
+
+Test: "It should return price of pizza depending on sauce that was selected and add it to the size price."
+Code:
+  let pizza = new Pizza("Small", "Red Sauce", "Ranch")
+  pizza.saucePrice()
+Expected Return: 10
+
+Describe dippingPrice()
+
+Test: "It should return price of pizza depending on dip that was selected"
+Code:
+  let pizza = new Pizza("Small", "Red Sauce", "Ranch")
+  pizza.dippingPrice()
+Expected Return: 12
+
